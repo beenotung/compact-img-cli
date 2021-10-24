16714848a832b21fdea37778c5b48f4a8128620f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-10-25 01:48:15 +0800

    fix missing args in test

--- test/main.ts
+++ test/main.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env ts-node
 import { main } from '../src/main'
 import { KB } from '@beenotung/tslib/size'
+import { MINUTE } from '@beenotung/tslib/time';
 
 main({
   dir: 'res/sample',
   mode: 'once',
   backup_dir: 'res/backup',
   max_size: 800 * KB,
+  interval: 10 * MINUTE
 })
